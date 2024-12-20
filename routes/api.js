66f9e18a861796d1eeea90ad96cbd07ddc50ffb8
doasmike <doasmike@gmail.com>
2024-12-20 00:03:11 +0000

    Work in progress...

--- routes/api.js
+++ routes/api.js
@@ -7,41 +7,59 @@
 */
 
 'use strict';
+require('dotenv').config()
+const { default: mongoose, mongo } = require('mongoose');
 
 module.exports = function (app) {
 
+  mongoose.connect(process.env.MONGO_URI);
+
+  const bookSchema = new mongoose.Schema({
+    book_title: { type: String, required: true },
+    comments: [{ type: mongoose.Schema.Types.ObjectId, ref: 'Comment' }]
+  });
+
+  let Book = mongoose.model('Book', bookSchema);
+
+  const commentSchema = new mongoose.Schema({
+    comment: { type: String, required: true },
+    bookId: { type: mongoose.Schema.Types.ObjectId, ref: 'Book', , required: true}
+  });
+
+  let Comment = mongoose.model('Comment', commentSchema);
+
   app.route('/api/books')
-    .get(function (req, res){
+    .get(function (req, res) {
       //response will be array of book objects
       //json res format: [{"_id": bookid, "title": book_title, "commentcount": num_of_comments },...]
     })
-    
-    .post(function (req, res){
+
+    .post(function (req, res) {
       let title = req.body.title;
       //response will contain new book object including atleast _id and title
     })
-    
-    .delete(function(req, res){
+
+    .delete(function (req, res) {
       //if successful response will be 'complete delete successful'
     });
 
 
 
   app.route('/api/books/:id')
-    .get(function (req, res){
+    .get(function (req, res) {
       let bookid = req.params.id;
       //json res format: {"_id": bookid, "title": book_title, "comments": [comment,comment,...]}
     })
-    
-    .post(function(req, res){
+
+    .post(function (req, res) {
       let bookid = req.params.id;
       let comment = req.body.comment;
       //json res format same as .get
     })
-    
-    .delete(function(req, res){
+
+    .delete(function (req, res) {
       let bookid = req.params.id;
       //if successful response will be 'delete successful'
     });
-  
+
 };
